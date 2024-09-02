2850bd0365f735a7dd193fb08f4e914cb0048119
aymonsalver <aymonsalver@unal.edu.co>
2024-09-01 20:59:25 -0500

    validacion formulario login

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ValidacionService } from 'src/app/services/validacion.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class AppSideLoginComponent {
-  constructor() {}
+export class AppSideLoginComponent implements OnInit{
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private vs: ValidacionService
+  ) {
+    this.loginForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email, this.vs.dominioUnal.bind(this.vs)]],
+      contrasena: ['', Validators.required],
+    })
+  }
+
+  ngOnInit(): void { }
+
+  irAHome() {
+    if (this.loginForm.valid) {
+      // Aquí puedes manejar la lógica de envío o redirección
+      this.router.navigate([`/home`]);
+    } else {
+      console.log("Formulario inválido, por favor revise los campos.");
+    }
+  }
+
+
+  getErrorMessage(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    if (!control) {
+      return '';
+    }
+    return this.vs.getErrorMessage(control, controlName);
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
