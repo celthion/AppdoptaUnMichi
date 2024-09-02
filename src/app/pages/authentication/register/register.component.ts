7ace55eaa516ed4c8e088fe472396c5bea7ae374
aymonsalver <aymonsalver@unal.edu.co>
2024-09-01 20:58:54 -0500

    validacion formulario registro

--- src/app/pages/authentication/register/register.component.ts
+++ src/app/pages/authentication/register/register.component.ts
@@ -1,26 +1,57 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+import { ValidacionService } from 'src/app/services/validacion.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
-export class AppSideRegisterComponent {
-  constructor(private router: Router) {}
+export class AppSideRegisterComponent implements OnInit{
+  registerForm: FormGroup;
 
-  form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-  });
-
-  get f() {
-    return this.form.controls;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private vs: ValidacionService
+  ) {
+    this.registerForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.pattern(/^(?!\s*$)(?!.*\s{2,})[a-zA-Z\s]+$/)]],
+      correo: ['', [Validators.required, Validators.email, this.vs.dominioUnal.bind(this.vs)]],
+      contrasena: ['', Validators.required],
+    })
   }
 
-  submit() {
+
+/*   submit() {
     // console.log(this.form.value);
     this.router.navigate(['/home']);
+  } */
+
+  ngOnInit(): void { }
+
+  irALogin() {
+    if (this.registerForm.valid) {
+      // Aquí puedes manejar la lógica de envío o redirección
+      this.router.navigate([`/authentication/login`]);
+    } else {
+      console.log("Formulario inválido, por favor revise los campos.");
+    }
+  }
+
+
+  getErrorMessage(controlName: string): string {
+    const control = this.registerForm.get(controlName);
+    if (!control) {
+      return '';
+    }
+    return this.vs.getErrorMessage(control, controlName);
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 }
